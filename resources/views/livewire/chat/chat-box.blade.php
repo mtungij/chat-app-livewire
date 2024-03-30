c11a0376a68964576d802f6c5a2ff4e7b81ce71d
unknown <jamesmtungi2013@gmail.com>
2024-03-29 17:06:32 -0700

    first commit

---
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -0,0 +1,21 @@
+<div>
+   <header class="w-full sticky  inset-x-0 flex pb-[5px] pt-[5px] top-0 z-10 bg-white border-b">
+
+    <div class="flex w-full items-center px-2 lg:px-4 gap-2 md:gap-5">
+
+        <a href="http://" class="shrink-0 lg:hidden ">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+              </svg>
+        </a>
+
+        <div class="shrink-0">
+
+           <x-avatar class="h-9 w-9 lg:w-11 lg:h-11"/>
+        </div>
+        <h6 class="font-bold truncate">{{fake()->name}}</h6>
+    </div>
+
+
+   </header>
+</div>
